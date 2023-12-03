0b05a1b5de814b32151b8d0c7d8a92f8089bc30f
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-04 00:11:54 +0800

    Update PoolStakedToken.tsx

--- apps/orne-dapp/src/components/earn/PoolStakedToken.tsx
+++ apps/orne-dapp/src/components/earn/PoolStakedToken.tsx
@@ -1,24 +1,25 @@
 import { ThreeDots } from 'react-loader-spinner';
 import { useLPBalance } from '~/hooks/useLPBalance';
+import { useOrneBalance } from '~/hooks/useOrneBalances';
 import { readAmounts } from '~/utils/readAmounts';
 import { readAmounts } from '~/utils/readAmounts';
 
 export function PoolStakedToken() {
 	const { data: balance, isLoading: isLoadingBalance } = useLPBalance();
-
+  const orneBalance = useOrneBalance();
 	return (
 		<div className="flex flex-col gap-2">
-			<span className="text-darkBlue50">Stacked</span>
+			<span className="text-darkBlue50">My Balance</span>
 			<div className="relative flex flex-col">
 				{isLoadingBalance ? (
 					<ThreeDots color="hsl(203,23%,42%)" height="10" />
 				) : (
 					<>
 						<span className="text-2xl font-semibold">
-							~{readAmounts(balance?.stakedLPBalance)} <span className="text-base font-normal">uLP</span>
+							~{readAmounts(orneBalance.data?.balance)} <span className="text-base font-normal">SEUL</span>
 						</span>
-						<span className="text-green absolute -bottom-5 right-0 w-max text-sm">
-							+ {readAmounts(balance?.lpBalance)} unstaked
+						<span className="text-2xl font-semibold absolute -bottom-5 right-0 w-max text-sm">
+							+ {readAmounts(balance?.lpBalance)} xSEUL
 						</span>
 					</>
 				)}
