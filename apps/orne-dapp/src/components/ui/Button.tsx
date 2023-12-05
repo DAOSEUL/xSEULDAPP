f804fab70bca764b584dd1a9aaa7fee01c5f0bfc
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-05 17:45:29 +0800

    Update Button.tsx

--- apps/orne-dapp/src/components/ui/Button.tsx
+++ apps/orne-dapp/src/components/ui/Button.tsx
@@ -16,18 +16,17 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button
 	ref
 ) {
 	const classes = clsx(
-		'inline-flex items-center justify-center font-semibold px-4 py-2 border border-transparent rounded-full transition-colors',
-		{
-			'bg-green text-darkGreen hover:bg-green75': variant === 'fill',
-			'text-green border-2 !border-green hover:bg-green hover:text-darkGreen': variant === 'outline',
-		},
-		{
-			'h-8 px-3 text-sm': size === 'sm',
-			'h-12 w-full text-base': size === 'lg',
-		},
-		className
-	);
-
+  'inline-flex items-center justify-center font-semibold px-4 py-2 border border-transparent rounded-full transition-colors',
+  {
+    'bg-slate-900 text-sky-500 hover:bg-slate-800 hover:text-sky-400': variant === 'fill', // Ganti warna background dan teks di sini
+    'text-green border-2 !border-green hover:bg-green hover:text-darkGreen': variant === 'outline',
+  },
+  {
+    'h-8 px-3 text-sm': size === 'sm',
+    'h-12 w-full text-base': size === 'lg',
+  },
+  className
+);
 	return (
 		<button className={classes} ref={ref} type={type} onClick={onClick}>
 			{children}
