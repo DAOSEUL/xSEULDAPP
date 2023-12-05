8503c0254d620ecb795c91f76c01f61543b226a5
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-05 17:24:43 +0800

    Update Earn.tsx

--- apps/orne-dapp/src/pages/Earn.tsx
+++ apps/orne-dapp/src/pages/Earn.tsx
@@ -6,46 +6,46 @@ import { WithdrawForm } from '~/components/earn/WithdrawForm';
 import { Button } from '~/components/ui/Button';
 
 export function Earn() {
-	const { status } = useWallet();
-	const [openForm, setOpenForm] = useState<'provide' | 'withdraw'>('provide');
+  const { status } = useWallet();
+  const [openForm, setOpenForm] = useState<'provide' | 'withdraw'>('provide');
 
-	return (
-		<div className="mt-5 lg:-mt-6">
-			<div className="mb-10 text-center lg:mb-20 lg:text-left">
-				<h1 className="mb-5 text-5xl font-bold">
-					<span className="earn-underline">MINT</span> BURN
-				</h1>
-				<h2 className="text-2xl">
-					SEUL <span className="text-green">xSEUL</span>
-				</h2>
-			</div>
+  return (
+    <div className="mt-5 lg:-mt-6 bg-slate-900"> {/* Add the bg-slate-900 class here */}
+      <div className="mb-10 text-center lg:mb-20 lg:text-left">
+        <h1 className="mb-5 text-5xl font-bold">
+          <span className="earn-underline">MINT</span> BURN
+        </h1>
+        <h2 className="text-2xl">
+          SEUL <span className="text-green">xSEUL</span>
+        </h2>
+      </div>
 
-			<PoolInfo />
+      <PoolInfo />
 
-			{status === WalletStatus.WALLET_CONNECTED && (
-				<>
-					<div className="mb-20 flex w-full justify-end gap-3 p-5">
-						<Button
-							className="w-full lg:w-1/5"
-							variant={openForm === 'provide' ? 'fill' : 'outline'}
-							onClick={() => setOpenForm('provide')}
-						>
-							Mint xSEUL
-						</Button>
+      {status === WalletStatus.WALLET_CONNECTED && (
+        <>
+          <div className="mb-20 flex w-full justify-end gap-3 p-5">
+            <Button
+              className="w-full lg:w-1/5"
+              variant={openForm === 'provide' ? 'fill' : 'outline'}
+              onClick={() => setOpenForm('provide')}
+            >
+              Mint xSEUL
+            </Button>
 
-						<Button
-							className="w-full lg:w-1/5"
-							variant={openForm === 'withdraw' ? 'fill' : 'outline'}
-							onClick={() => setOpenForm('withdraw')}
-						>
-							Burn xSEUL
-						</Button>
-					</div>
+            <Button
+              className="w-full lg:w-1/5"
+              variant={openForm === 'withdraw' ? 'fill' : 'outline'}
+              onClick={() => setOpenForm('withdraw')}
+            >
+              Burn xSEUL
+            </Button>
+          </div>
 
-					{openForm === 'provide' && <ProvideForm />}
-					{openForm === 'withdraw' && <WithdrawForm />}
-				</>
-			)}
-		</div>
-	);
+          {openForm === 'provide' && <ProvideForm />}
+          {openForm === 'withdraw' && <WithdrawForm />}
+        </>
+      )}
+    </div>
+  );
 }
