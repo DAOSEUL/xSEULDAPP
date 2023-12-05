3f923d21a0d471b5dd65fb5fccd4a8ddda43a0a7
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-05 16:05:48 +0800

    Update WithdrawForm.tsx

--- apps/orne-dapp/src/components/earn/WithdrawForm.tsx
+++ apps/orne-dapp/src/components/earn/WithdrawForm.tsx
@@ -50,7 +50,7 @@ export function WithdrawForm() {
       <div className="w-full flex-1">
         <div className="bg-offWhite flex h-32 flex-1 flex-col justify-center rounded-lg p-8 shadow-sm">
           <div className="flex w-full justify-between">
-            <span className="text-darkBlue50 mb-3">Balance</span>
+            <span className="text-darkBlue50 mb-3">Balance xSEUL</span>
             <div className="-mt-2 flex items-center gap-2">
               <span className="text-darkBlue50">
                 {orneBalance.isLoading ? (
