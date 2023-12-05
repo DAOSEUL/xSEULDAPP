1f9c245a7ca135d624826706196f426da96b19d0
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-05 20:52:59 +0800

    Update WithdrawForm.tsx

--- apps/orne-dapp/src/components/earn/WithdrawForm.tsx
+++ apps/orne-dapp/src/components/earn/WithdrawForm.tsx
@@ -44,11 +44,11 @@ export function WithdrawForm() {
 		<>
 			<div className="mb-5 flex items-center gap-2">
 				<h2 className="text-3xl font-semibold">
-					Burn <span className="text-green">xSEUL</span>
+					Burn <span className="text-sky-500">xSEUL</span>
 				</h2>
 				<button
 					type="button"
-					className="border-green bg-slate-90025 hover:bg-slate-900 flex h-7 items-center justify-center rounded-lg border px-3 font-semibold transition-colors hover:text-white"
+					className="border-sky-500 bg-slate-90025 hover:bg-slate-900 flex h-7 items-center justify-center rounded-lg border px-3 font-semibold transition-colors hover:text-white"
 					onClick={handleMaxClick}
 				>
 					Max
@@ -57,11 +57,11 @@ export function WithdrawForm() {
 
 			<div className="mb-10 flex flex-col items-center gap-8">
 				<div className="w-full flex-1">
-					<div className="bg-offWhite flex h-32 flex-1 flex-col justify-center rounded-lg p-8 shadow-sm">
+					<div className="bg-slate-900 flex h-32 flex-1 flex-col justify-center rounded-lg p-8 shadow-sm">
 						<div className="flex w-full justify-between">
-							<span className="text-darkBlue50 mb-3">Balance</span>
+							<span className="text-sky-500 mb-3">Balance</span>
 							<div className="-mt-2 flex items-center gap-2">
-								<span className="text-darkBlue50">
+								<span className="text-sky-500">
 									{isLoadingOrneBalance ? (
 										<ThreeDots color="hsl(203,23%,42%)" height="10" />
 									) : (
@@ -71,7 +71,7 @@ export function WithdrawForm() {
 							</div>
 						</div>
 						<div className="flex justify-between">
-							<AmountInput className="bg-offWhite text-2xl font-semibold" value={amount} onChange={setAmount} />
+							<AmountInput className="bg-slate-900 text-2xl font-semibold" value={amount} onChange={setAmount} />
 							{/* Removed columns for ORNE and LUNA */}
 						</div>
 					</div>
