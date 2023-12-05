952fe171c6a2c2f2915170c466b0aef20d5398e0
Seul DAO team <152189007+DAOSEUL@users.noreply.github.com>
2023-12-05 16:51:42 +0800

    Update WithdrawForm.tsx

--- apps/orne-dapp/src/components/earn/WithdrawForm.tsx
+++ apps/orne-dapp/src/components/earn/WithdrawForm.tsx
@@ -11,12 +11,11 @@ import { useShare } from '~/hooks/useShare';
 import { useWithdrawLiquidity } from '~/hooks/useWithdrawLiquidity';
 import { Token } from '~/utils/constants';
 import { readAmounts } from '~/utils/readAmounts';
-import { readAmounts } from '~/utils/readAmounts';
 import { Button } from '../ui/Button';
 import { IconToken } from '../ui/IconToken';
 
 export function WithdrawForm() {
-	const orneBalance = useOrneBalance();
+	const { data: orneBalance, isLoading: isLoadingOrneBalance } = useOrneBalance();
   const { data: balance, isLoading: isLoadingBalance } = useLPBalance();
 	
 	const [amount, setAmount] = useState('');
@@ -24,9 +23,15 @@ export function WithdrawForm() {
 	const { data: withdrawing, isLoading: isLoadingShare } = useShare(+debouncedAmount);
 	const { mutate: withdraw } = useWithdrawLiquidity();
 
+	const handleMaxClick = () => {
+		if (orneBalance) {
+			setAmount(readAmounts(orneBalance?.balance));
+		}
+	};
+
 	function handleSubmit() {
 		withdraw(
-			{ amount: new Dec(string.transformToValidInput(amount)) },
+			{ amount: new Dec(string.transformToValidInput(debouncedAmount)) },
 			{
 				onSuccess() {
 					setAmount('');
@@ -36,49 +41,50 @@ export function WithdrawForm() {
 	}
 
 	return (
-  <>
-    <div className="mb-5 flex items-center gap-2">
-      <h2 className="text-3xl font-semibold">
-        Burn <span className="text-green">xSEUL</span>
-      </h2>
-      {/* <button className="border-green bg-green25 hover:bg-green flex h-7 items-center justify-center rounded-lg border px-3 font-semibold transition-colors hover:text-white">
-        Max
-      </button> */}
-    </div>
+		<>
+			<div className="mb-5 flex items-center gap-2">
+				<h2 className="text-3xl font-semibold">
+					Burn <span className="text-green">xSEUL</span>
+				</h2>
+				<button
+					type="button"
+					className="border-green bg-green25 hover:bg-green flex h-7 items-center justify-center rounded-lg border px-3 font-semibold transition-colors hover:text-white"
+					onClick={handleMaxClick}
+				>
+					Max
+				</button>
+			</div>
 
-    <div className="mb-10 flex flex-col items-center gap-8">
-      <div className="w-full flex-1">
-        <div className="bg-offWhite flex h-32 flex-1 flex-col justify-center rounded-lg p-8 shadow-sm">
-          <div className="flex w-full justify-between">
-            <span className="text-darkBlue50 mb-3">Balance</span>
-            <div className="-mt-2 flex items-center gap-2">
-              <span className="text-darkBlue50">
-                {orneBalance.isLoading ? (
-                    <ThreeDots color="hsl(203,23%,42%)" height="10" />
-                  ) : (
-                    readAmounts(orneBalance.data?.balance)
-                  )}
-              </span>
-              {/* <button className="border-green bg-green25 hover:bg-green flex h-7 items-center justify-center rounded-lg border px-3 font-semibold transition-colors hover:text-white">
-                Max
-              </button> */}
-            </div>
-          </div>
-          <div className="flex justify-between">
-            <AmountInput className="bg-offWhite text-2xl font-semibold" value={amount} onChange={setAmount} />
-            {/* Removed columns for ORNE and LUNA */}
-          </div>
-        </div>
-      </div>
+			<div className="mb-10 flex flex-col items-center gap-8">
+				<div className="w-full flex-1">
+					<div className="bg-offWhite flex h-32 flex-1 flex-col justify-center rounded-lg p-8 shadow-sm">
+						<div className="flex w-full justify-between">
+							<span className="text-darkBlue50 mb-3">Balance</span>
+							<div className="-mt-2 flex items-center gap-2">
+								<span className="text-darkBlue50">
+									{isLoadingOrneBalance ? (
+										<ThreeDots color="hsl(203,23%,42%)" height="10" />
+									) : (
+										readAmounts(orneBalance?.balance)
+									)}
+								</span>
+							</div>
+						</div>
+						<div className="flex justify-between">
+							<AmountInput className="bg-offWhite text-2xl font-semibold" value={amount} onChange={setAmount} />
+							{/* Removed columns for ORNE and LUNA */}
+						</div>
+					</div>
+				</div>
 
-      {/* Removed "Swap currency" section */}
-      
-      {/* Removed columns for ORNE and LUNA */}
-    </div>
+				{/* Removed "Swap currency" section */}
 
-    <Button className="mb-14" onClick={handleSubmit}>
-      Burn xSEUL
-    </Button>
-  </>
-);
+				{/* Removed columns for ORNE and LUNA */}
+			</div>
+
+			<Button className="mb-14" onClick={handleSubmit}>
+				Burn xSEUL
+			</Button>
+		</>
+	);
 };
